2c6bc2621fff3aba7e4e27d4eefa9c5346159801
Shakira <sj7johnson@students.latrobe.edu.au>
2017-05-23 14:16:09 +1000

    Return NA, not 0, on invalid input

--- strcalc.R
+++ strcalc.R
@@ -3,7 +3,7 @@ compute = function(input_string) {
   num0 = as.integer(values[1])
   operator = values[2]
   num1 = as.integer(values[3])
-  if (operator == '+') {
+  if (operator == '-') {
     return(num0 + num1)
   } else {
     print('unknown operator!')

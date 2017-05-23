4094428a3a0109ea8922b7ffa0d357d967d7ee6f
Shakira <sj7johnson@students.latrobe.edu.au>
2017-05-23 15:20:53 +1000

    Add support for division operator

--- strcalc.R
+++ strcalc.R
@@ -1,16 +1,16 @@
 compute = function(input_string) {
-  # Perform simple arithmetic encoded in input string
-  # e.g. '1 + 2' -> 3, or '1 - 2' -> -1
   values = unlist(strsplit(input_string, ' '))
   num0 = as.integer(values[1])
   operator = values[2]
   num1 = as.integer(values[3])
-  if (operator == '+') {
-    return(num0 + num1)
-  } else if (operator == '-') {
-    return(num0 - num1)
-  } else {
+  result = switch(operator,
+                  '+' = num0 + num1,
+                  '-' = num0 - num1,
+                  '*' = num0 * num1,
+                  '/' = num0 * num1,
+                  NA)
+  if (is.na(result)) {
     print('unknown operator!')
-    return(NA)
   }
+  return(result)
 }
